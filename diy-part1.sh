e778dcc349736f3c4e64fcb23524102e653da01f
zhuksmu <60605055+zhuksmu@users.noreply.github.com>
2021-07-01 09:25:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,13 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+ sed -i '$a src-git helloworld https://github.com/fw876/helloworld' ./feeds.conf.default
 
-# fw876/helloworld
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 添加额外软件包
+git clone https://github.com/jerrykuku/lua-maxminddb.git ./package/lean/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git ./package/lean/luci-app-vssr
+git clone https://github.com/xiaorouji/openwrt-passwall.git ./package/lean/openwrt-passwall
+git clone https://github.com/fw876/helloworld.git ./package/lean/helloworld
+
+# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
